529792d29a30a972ac66ca27c3a216a845e2d5f2
Tayyaba <tayyaba.asad1234@gmail.com>
2020-12-10 13:03:23 -0500

    updated express.js file

---
+++ express.js
@@ -0,0 +1,20 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+const app = express();
+const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// res.send("Welcome to the Index page!")
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+// res.send("Welcome to the Notes page!")
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+
