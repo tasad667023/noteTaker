1efa24db3bf9ce3bd14ab9197adf63b97925cef0
Tayyaba <tayyaba.asad1234@gmail.com>
2020-12-17 14:02:48 -0500

    prepping for heroku deployment

--- express.js
+++ express.js
@@ -6,7 +6,7 @@ const db = require("./db/db.json");
 
 // Sets up the Express App
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
